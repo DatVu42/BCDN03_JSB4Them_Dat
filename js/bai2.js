c7d90356e88bff5938eac4671a3b970aaef8f28b
DatVu42 <vudat42@gmail.com>
2022-05-08 07:53:51 +0700

    update bai 3

--- js/bai2.js
+++ js/bai2.js
@@ -1,4 +1,18 @@
-
+/**
+ * Input: month/year
+ * 
+ * Các bước xử lý:
+ * B1: Tìm các thẻ bằng id
+ * B2: Tạo hàm tính
+ *    + monthYear = new Date(inpMonthYear.value)
+ *       month = monthYear.getMonth() + 1
+ *       year = monthYear.getFullYear()
+ *       days = new Date(year, month, 0).getDate()
+ *    + Hiển thị kết quả lên UI
+ * B3: Gắn hàm vào sự kiện click
+ * 
+ * Output: days
+ */
 
 var inpMonthYear = document.getElementById("monthYear");
 
